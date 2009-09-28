6f96e82bf34928a5a14ba32482820a6d7729b735
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2009-09-28 15:37:05 +0000

    new files
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/Timbl6@2981 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ include/timbl/XMLtools.h
@@ -0,0 +1,46 @@
+#ifndef XML_TOOLS_H
+#define XML_TOOLS_H
+
+#include <list>
+#include "libxml/tree.h"
+
+xmlNode *XmlNewNode( const std::string& );
+xmlNode *XmlNewComment( const std::string& );
+xmlNode *XmlNewChild( xmlNode *, 
+		      const std::string& , const std::string& ="" );
+xmlNode *XmlAddChild( xmlNode *, xmlNode * );
+void XmlAddContent( xmlNode *, const std::string& );
+
+bool XmlSetAttribute( xmlNode *, const std::string&, const std::string& );
+
+std::string serialize( const xmlNode& node );
+
+class XmlDoc {
+  friend std::ostream& operator << ( std::ostream& , const XmlDoc& );
+ public:
+  XmlDoc( const std::string& );
+  ~XmlDoc( );
+  void setRoot( xmlNode* );
+  xmlNode *getRoot() const;
+  xmlNode *MakeRoot( const std::string& );
+  std::string toString() const;
+ private:
+  xmlDoc *the_doc;
+};
+
+inline std::ostream& operator << ( std::ostream& os, const XmlDoc& doc ){
+  os << doc.toString();
+  return os;
+}
+
+inline std::ostream& operator << ( std::ostream& os, const xmlNode& node ){
+  os << serialize( node );
+  return os;
+}
+
+inline std::ostream& operator << ( std::ostream& os, const xmlNode *node ){
+  os << serialize( *node );
+  return os;
+}
+
+#endif
---
+++ src/XMLtools.cxx
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include "timbl/XMLtools.h"
+
+using namespace std;
+
+XmlDoc::XmlDoc( const std::string& elem ){
+  the_doc = xmlNewDoc( (const xmlChar*)"""1.0""" );
+  MakeRoot( elem );
+}
+
+XmlDoc::~XmlDoc(){
+  xmlFreeDoc( the_doc );
+}
+
+string XmlDoc::toString() const {
+  xmlChar *buf; 
+  int size;
+  xmlDocDumpMemory( the_doc, &buf, &size );
+  string result = string( (const char *)buf, size );
+  xmlFree( buf );
+  return result;
+}
+
+xmlNode *XmlDoc::getRoot() const {
+  if ( the_doc )
+    return xmlDocGetRootElement(the_doc);
+  else
+    return 0;
+}
+
+void XmlDoc::setRoot( xmlNode *node ){
+  if ( the_doc )
+    xmlDocSetRootElement(the_doc, node );
+}
+
+xmlNode *XmlDoc::MakeRoot( const string& elem ){
+  xmlNode *root;
+  root = xmlNewDocNode( the_doc, 0, (const xmlChar*)elem.c_str(), 0 );
+  xmlDocSetRootElement( the_doc, root );
+  return root;
+}
+
+xmlNode *XmlNewNode( const string& elem ){
+  return xmlNewNode( 0, (const xmlChar*)elem.c_str() );
+}
+
+xmlNode *XmlNewComment( const string& elem ){
+  return xmlNewComment( (const xmlChar*)elem.c_str() );
+}
+
+xmlNode *XmlNewChild( xmlNode *node, const string& elem, const string& val ){
+  if ( val.empty() )
+    return xmlNewTextChild( node, 0, (xmlChar*)elem.c_str(), 0 );
+  else 
+    return xmlNewTextChild( node, 0, 
+			    (const xmlChar*)elem.c_str(),
+			    (const xmlChar*)val.c_str() );
+}
+
+xmlNode *XmlAddChild( xmlNode *node, xmlNode *elem ){
+  return xmlAddChild( node, elem );
+}
+
+void XmlAddContent( xmlNode *node, const string& cont ){
+  xmlNodeAddContent( node, (const xmlChar*)cont.c_str() );
+}
+
+bool XmlSetAttribute( xmlNode *node, 
+		      const std::string& att,
+		      const std::string& val ){
+  if ( node ){
+    return xmlSetProp( node, 
+		       (const xmlChar*)att.c_str(), 
+		       (const xmlChar*)val.c_str() )!= 0;
+  }
+  else
+    return false;
+}
+
+string serialize( const xmlNode& node ){
+  XmlDoc tmp( "root" );
+  xmlAddChild( tmp.getRoot(), xmlCopyNode( const_cast<xmlNode*>(&node), 1 ) );
+  string root = tmp.toString();
+  string::size_type s_pos = root.find( "<root>" );
+  string::size_type e_pos = root.find( "</root>" );
+  return root.substr( s_pos+6, e_pos-s_pos-6 );
+}
