7de47e82e520c4a0d86cef30b8f472b2730baade
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-06 14:17:11 +0000

    IndexClass was not used added Copyright messages
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/Timbl6@7151 12f355fe-0486-481a-ad91-c297ab22b4e3

--- include/timbl/XMLtools.h
+++ include/timbl/XMLtools.h
@@ -1,3 +1,29 @@
+/*
+  Copyright (c) 1998 - 2010
+  ILK  -  Tilburg University
+  CNTS -  University of Antwerp
+ 
+  This file is part of Timbl
+
+  Timbl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  Timbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      http://ilk.uvt.nl/software.html
+  or send mail to:
+      Timbl@uvt.nl
+*/
+
 #ifndef XML_TOOLS_H
 #define XML_TOOLS_H
 
