2a0cba4d3eccf40d056fe21b369e90f32a956d9e
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-09-13 12:18:56 +0000

    added a test. 'make check' and 'make distcheck' now work
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/Timbl6@13150 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ src/simpletest.cxx
@@ -0,0 +1,45 @@
+/*
+  Copyright (c) 1998 - 2011
+  ILK   - Tilburg University
+  CLiPS - University of Antwerp
+ 
+  This file is part of timbl
+
+  timbl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  timbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      http://ilk.uvt.nl/software.html
+  or send mail to:
+      timbl@uvt.nl
+*/
+
+#include "timbl/TimblAPI.h"
+#include <cstdlib>
+#include "config.h"
+
+int main(){
+  std::string path = std::getenv( "topsrcdir" );
+  std::cerr << path << std::endl;
+  
+  Timbl::TimblAPI exp( "+vdi+db", "test1" );
+  if ( exp.isValid() ){
+    exp.Learn( path + "/demos/dimin.train" );  
+    if ( exp.isValid() ){
+      exp.Test( path + "/demos/dimin.test", "dimin.out" );
+      if ( exp.isValid() )
+	return EXIT_SUCCESS;
+    }
+  }
+  return EXIT_FAILURE;
+}
