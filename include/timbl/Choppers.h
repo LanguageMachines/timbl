b97b58558c9781042ada4bb6cb58051cd04c8cba
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-12-20 16:33:46 +0100

    bump year

--- include/timbl/Choppers.h
+++ include/timbl/Choppers.h
@@ -1,7 +1,7 @@
 #ifndef TIMBL_CHOPPERS_H
 #define TIMBL_CHOPPERS_H
 /*
-  Copyright (c) 1998 - 2016
+  Copyright (c) 1998 - 2017
   ILK   - Tilburg University
   CLST  - Radboud University
   CLiPS - University of Antwerp
--- src/MBLClass.cxx
+++ src/MBLClass.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 1998 - 2016
+  Copyright (c) 1998 - 2017
   ILK   - Tilburg University
   CLST  - Radboud University
   CLiPS - University of Antwerp
