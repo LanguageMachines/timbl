489ad0bd9c04f7a02ea6a3c831b3bdbcc25cbb83
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2021-11-16 19:08:43 +0100

    syntax error.not seen by GCC?

--- src/TimblExperiment.cxx
+++ src/TimblExperiment.cxx
@@ -2046,7 +2046,7 @@ namespace Timbl {
 	if ( !chopLine( Buffer ) ) {
 	  Warning( "testfile, skipped line #" +
 		   TiCC::toString<int>( stats.totalLines() ) +
-		   "\n" + TiCC::UnicodeToUTF8(Buffer);
+		   "\n" + TiCC::UnicodeToUTF8(Buffer) );
 	}
 	else {
 	  chopped_to_instance( TestWords );
