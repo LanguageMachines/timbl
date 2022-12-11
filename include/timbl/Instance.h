bd523e5756f6849b03ed71996ed14590c2e6667b
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2022-12-11 10:29:03 +0100

    reshuffle code

--- include/timbl/Instance.h
+++ include/timbl/Instance.h
@@ -34,8 +34,9 @@
 #include <map>
 #include <unordered_map>
 #include "unicode/unistr.h"
-#include "timbl/MsgClass.h"
 #include "ticcutils/Unicode.h"
+#include "timbl/Targets.h"
+#include "timbl/Features.h"
 
 namespace Hash {
   class UnicodeHash;
@@ -45,106 +46,6 @@ namespace Timbl {
 
   class TargetValue;
   class FeatureValue;
-
-  class Vfield{
-    friend class ValueDistribution;
-    friend class WValueDistribution;
-    friend std::ostream& operator<<( std::ostream&, const Vfield& );
-    friend std::ostream& operator<<( std::ostream&, const Vfield * );
-  public:
-    Vfield( const TargetValue *val, int freq, double w ):
-      value(val), frequency(freq), weight(w) {};
-    Vfield( const Vfield& in ):
-      value(in.value), frequency(in.frequency), weight(in.weight) {};
-    Vfield& operator=( const Vfield& ) = delete; // forbid copies
-    ~Vfield(){};
-    std::ostream& put( std::ostream& ) const;
-    const TargetValue *Value() const { return value; };
-    void Value( const TargetValue *t ){  value = t; };
-    size_t Freq() const { return frequency; };
-    void IncFreq( int inc=1 ) {  frequency += inc; };
-    void AddFreq( int f ) {  frequency += f; weight += f; };
-    void DecFreq() {  frequency -= 1; };
-    double Weight() const { return weight; };
-    void SetWeight( double w ){ weight = w; };
-    size_t Index();
-  protected:
-    const TargetValue *value;
-    size_t frequency;
-    double weight;
-  private:
-  };
-
-  class Targets;
-
-  class WValueDistribution;
-
-  class ValueDistribution{
-    friend std::ostream& operator<<( std::ostream&, const ValueDistribution& );
-    friend std::ostream& operator<<( std::ostream&, const ValueDistribution * );
-    friend class WValueDistribution;
-  public:
-    typedef std::map<size_t, Vfield *> VDlist;
-    typedef VDlist::const_iterator dist_iterator;
-    ValueDistribution( ): total_items(0) {};
-    ValueDistribution( const ValueDistribution& );
-    virtual ~ValueDistribution(){ clear(); };
-    size_t totalSize() const{ return total_items; };
-    size_t size() const{ return distribution.size(); };
-    bool empty() const{ return distribution.empty(); };
-    void clear();
-    dist_iterator begin() const { return distribution.begin(); };
-    dist_iterator end() const { return distribution.end(); };
-    virtual const TargetValue* BestTarget( bool&, bool = false ) const;
-    void Merge( const ValueDistribution& );
-    virtual void SetFreq( const TargetValue *, int, double=1.0 );
-    virtual bool IncFreq( const TargetValue *, size_t, double=1.0 );
-    void DecFreq( const TargetValue * );
-    static ValueDistribution *read_distribution( std::istream&,
-						 Targets&,
-						 bool );
-    static ValueDistribution *read_distribution_hashed( std::istream&,
-							Targets&,
-							bool );
-    const std::string DistToString() const;
-    const std::string DistToStringW( int ) const;
-    double Confidence( const TargetValue * ) const;
-    virtual const std::string SaveHashed() const;
-    virtual const std::string Save() const;
-    bool ZeroDist() const { return total_items == 0; };
-    double Entropy() const;
-    ValueDistribution *to_VD_Copy( ) const;
-    virtual WValueDistribution *to_WVD_Copy() const;
-  protected:
-    virtual void DistToString( std::string&, double=0 ) const;
-    virtual void DistToStringWW( std::string&, int ) const;
-    const TargetValue* BestTargetN( bool &, bool = false ) const;
-    const TargetValue* BestTargetW( bool &, bool = false ) const;
-    virtual ValueDistribution *clone( ) const {
-      return new ValueDistribution(); };
-    size_t total_items;
-    VDlist distribution;
-  };
-
-  class WValueDistribution: public ValueDistribution {
-  public:
-    WValueDistribution(): ValueDistribution() {};
-    const TargetValue* BestTarget( bool &, bool = false ) const override;
-    void SetFreq( const TargetValue *, int, double ) override;
-    bool IncFreq( const TargetValue *, size_t, double ) override;
-    WValueDistribution *to_WVD_Copy( ) const override;
-    const std::string SaveHashed() const override;
-    const std::string Save() const override;
-    void Normalize();
-    void Normalize_1( double, const Targets& );
-    void Normalize_2();
-    void MergeW( const ValueDistribution&, double );
-  private:
-    void DistToString( std::string&, double=0 ) const override;
-    void DistToStringWW( std::string&, int ) const override;
-    WValueDistribution *clone() const override {
-      return new WValueDistribution; };
-  };
 
   class Instance {
     friend std::ostream& operator<<(std::ostream&, const Instance& );
