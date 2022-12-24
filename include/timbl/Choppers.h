8d152e541a1e973349088ca379a6b38a8e5c0f59
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2022-12-24 10:02:09 +0100

    leaping into 2023

--- include/timbl/Choppers.h
+++ include/timbl/Choppers.h
@@ -1,7 +1,7 @@
 #ifndef TIMBL_CHOPPERS_H
 #define TIMBL_CHOPPERS_H
 /*
-  Copyright (c) 1998 - 2022
+  Copyright (c) 1998 - 2023
   ILK   - Tilburg University
   CLST  - Radboud University
   CLiPS - University of Antwerp
--- src/Timbl.cxx
+++ src/Timbl.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 1998 - 2022
+  Copyright (c) 1998 - 2023
   ILK   - Tilburg University
   CLST  - Radboud University
   CLiPS - University of Antwerp
